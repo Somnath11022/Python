8119f696f3fbcd8976aee6fce83af8a1e9ca8b75
Somnath Gaikwad <somnathgaikwad1102@gmail.com>
2023-03-03 12:27:19 +0400

    f commit

---
+++ Python_Programs/OOP/Numbers1.py
@@ -0,0 +1,46 @@
+#Write a Program aceept n numbers from user 
+#And Return Addition of that N numbers 
+
+class Numbers:
+
+    def __init__(self):
+        self.iSize = 0
+        self.Arr= list()
+
+    def Accept(self):
+        print("Enter a number to set Limit : ")
+        self.iSize = int(input())
+        print("Enter Data")
+        for i in range (0, self.iSize):
+                No = int(input())
+                self.Arr.append(No)
+    def Display(self):
+        print("Inserted Data is : ")
+        for i in range(0,self.iSize):
+            print(self.Arr[i])
+
+        #print(self.Arr)
+
+    def Summation(self):
+        self.Sum = 0
+        for i in range(0,self.iSize):
+            self.Sum += self.Arr[i]
+        return self.Sum
+
+    def Average(self):
+        return self.Sum/self.iSize
+                
+        
+        
+def main():
+    obj = Numbers()
+    obj.Accept()
+    obj.Display()
+    Output = obj.Summation()
+    print("Summation of all Elemens is :",Output)
+
+    Output = obj.Average()
+    print("Average of all Elemens is :",Output)
+if __name__ == "__main__":
+
+    main()
---
+++ README.md
@@ -0,0 +1,4 @@
+This Repository is consist of my python codes and it is consist of 
+Assignments  - It  consist of programs ranging from basic to intermediate level difficulty
+Python_Programs - It Consist of programs seperated by topic 
+Scripts - It is consist of Automation Scripts to do Specific Task
